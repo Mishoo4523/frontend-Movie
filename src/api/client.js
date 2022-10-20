8e04d126101c2e22f1b1dcf22b5828cddc65857b
Misho <102149794+Mishoo4523@users.noreply.github.com>
2022-10-20 11:00:28 +0300

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const client = axios.create({ https://ttretetertet.herokuapp.com/api"});
+const client = axios.create({ https://ttretetertet.herokuapp.com/api",
+});
 
 //const client = axios.create({ baseURL: "http://localhost:8000/api" });
 //https://ttretetertet.herokuapp.com/api/movie/top-rated
