64597a1a0667bc5cb177df29ea0b7301fa314c4f
Mishoo4523 <mishomurad@gmail.com>
2022-10-20 09:44:09 +0300

    modify base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const client = axios.create({ https://ttretetertet.herokuapp.com/api"});
 
-const client = axios.create({ baseURL: "http://localhost:8000/api" });
-//const client = axios.create({ baseURL: "http://admin:1qaz2wsx3edc@movieapp.gul7hz0.mongodb.net/?retryWrites=true&w=majority/movieapp" });
+/const client = axios.create({ baseURL: "http://localhost:8000/api" });
+//https://ttretetertet.herokuapp.com/api/movie/top-rated
+
+
 
 
 export default client;
